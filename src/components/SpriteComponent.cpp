be60e72ffa1241534ba306fb7ced3638f3ac58a7
CM <omegajms@gmail.com>
2023-10-28 13:03:29 -0400

    yesm

--- src/components/SpriteComponent.cpp
+++ src/components/SpriteComponent.cpp
@@ -4,15 +4,17 @@
 
 void SpriteComponent::setSizeAndCenterPoint(sf::Vector2f size, sf::Vector2f centerPos)
 {
+	//m_textureCoordinates = sf::Vector2(25, 25);
+
 	//Assume the window is 1600 x 1200
 	//TODO: don't assume this, you peon!
 	float windowHeight = 1200.f;
 	float windowWidth = 1600.f;
 
-	//TODO: make these actual coordinates on the sprite sheet based on the texture size
-	//We probably need to know how big the sprite is vs the size of the thing in order to do this properly.
-	sf::Vector2f spriteBottomLeft = sf::Vector2f(0.f, 0.f);
-	sf::Vector2f spriteUpperRight = sf::Vector2f(1.f, 1.f); //1 is "the entire width/height of the texture" ie the entire sprite sheet.
+	//Upper left of where we want to start the texture
+	//TODO: calculate this once and store it.
+	sf::Vector2f normalizedTextureCoordinates = sf::Vector2f((float)m_textureCoordinates.x / m_textureWidth, (float)m_textureCoordinates.y / m_textureHeight);
+	sf::Vector2f normalizedSpriteSize = sf::Vector2f(m_spriteWidth / m_textureWidth, m_spriteHeight / m_textureHeight); //Assumes texture is bigger than the sprite...
 
 	//TODO: make this not completely suck. Use matrices, you fool!! This sucks!
 	sf::Vector2f normalizedSize = sf::Vector2f(size.x / windowWidth, size.y / windowHeight);
@@ -21,10 +23,10 @@ void SpriteComponent::setSizeAndCenterPoint(sf::Vector2f size, sf::Vector2f cent
 
 	//Create the equivalent of two triangles taped together (a thing commonly known as a rectangle)
 	m_vertices = {
-		normalizedCenterPos.x + normalizedHalfSize.x, normalizedCenterPos.y + normalizedHalfSize.y, 0.f, spriteUpperRight.x, spriteUpperRight.y,
-		normalizedCenterPos.x + normalizedHalfSize.x, normalizedCenterPos.y - normalizedHalfSize.y, 0.f, spriteUpperRight.x, spriteBottomLeft.y,
-		normalizedCenterPos.x - normalizedHalfSize.x, normalizedCenterPos.y - normalizedHalfSize.y, 0.f, spriteBottomLeft.x, spriteBottomLeft.y,
-		normalizedCenterPos.x - normalizedHalfSize.x, normalizedCenterPos.y + normalizedHalfSize.y, 0.f, spriteBottomLeft.x,spriteUpperRight.y 
+		normalizedCenterPos.x + normalizedHalfSize.x, normalizedCenterPos.y + normalizedHalfSize.y, 0.f, normalizedTextureCoordinates.x + normalizedSpriteSize.x, normalizedTextureCoordinates.y + normalizedSpriteSize.y,
+		normalizedCenterPos.x + normalizedHalfSize.x, normalizedCenterPos.y - normalizedHalfSize.y, 0.f, normalizedTextureCoordinates.x + normalizedSpriteSize.x, normalizedTextureCoordinates.y,
+		normalizedCenterPos.x - normalizedHalfSize.x, normalizedCenterPos.y - normalizedHalfSize.y, 0.f, normalizedTextureCoordinates.x, normalizedTextureCoordinates.y,
+		normalizedCenterPos.x - normalizedHalfSize.x, normalizedCenterPos.y + normalizedHalfSize.y, 0.f, normalizedTextureCoordinates.x,normalizedTextureCoordinates.y + normalizedSpriteSize.y 
 	};
 
 	m_indices = { 0,1,3 , 1, 2, 3 };
--- src/components/SpriteComponent.hpp
+++ src/components/SpriteComponent.hpp
@@ -16,7 +16,14 @@ public:
 private:
 	std::vector<GLfloat> m_vertices;
 	std::vector<GLuint> m_indices;
-	sf::Vector2i m_textureCoordinates = sf::Vector2i(0, 0);
+	sf::Vector2i m_textureCoordinates = sf::Vector2i(25, 25);
+
+	float m_textureHeight = 50.f;
+	float m_textureWidth = 50.f;
+
+	float m_spriteHeight = 50.f;
+	float m_spriteWidth = 50.f;
+
 };
 
 #endif
